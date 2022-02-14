2c53a52edb4d5e4b1677444ec0470d33872925e8
vladyslavkosenko <vaduha91@gmail.com>
2022-02-14 14:32:46 +0200

    Removed extra lines

--- src/com/Main.java
+++ src/com/Main.java
@@ -29,19 +29,16 @@ public class Main {
         }
         System.out.println("Second task");
         System.out.println(fruitsArray);
-
         List<String> workersBrigade1 = new ArrayList<>();
         List<String> workersBrigade2 = new ArrayList<>();
         workersBrigade1.add("Jon");
         workersBrigade1.add("Bon");
         workersBrigade1.add("Vason");
         workersBrigade1.add("Oleg");
-
         workersBrigade2.add("Oleg");
         workersBrigade2.add("Vasysualiy");
         workersBrigade2.add("Roberto");
         workersBrigade2.add("Paul");
-
         workersBrigade1.retainAll(workersBrigade2);
         System.out.println("Third task");
         System.out.println(workersBrigade1);
